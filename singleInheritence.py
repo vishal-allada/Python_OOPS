73b04c02b9e4847ef20e3edafcb31906c372336c
vishal-allada <vishal.allada@tigeranalytics.com>
2022-12-05 11:38:23 +0530

    single inheritance

---
+++ singleInheritence.py
@@ -0,0 +1,17 @@
+class Apple:
+    manufacturer = "Apple Inc."
+    contactWebsite = "www.apple.com/contact"
+
+    def contactDetails(self):
+        print("To contact us, log on to",self.contactWebsite)
+
+class MacBook(Apple):
+    def __init__(self):
+        self.yearOfManufacturer = 2017
+    
+    def manufacturerDetails(self):
+        print("The Macbook was manufactured in the year {} by {}".format(self.yearOfManufacturer,self.manufacturer))
+
+macbook = MacBook()
+macbook.manufacturerDetails()
+macbook.contactDetails()
